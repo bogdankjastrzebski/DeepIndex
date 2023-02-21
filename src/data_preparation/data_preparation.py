113650f95d8d5b9cba537675723d4f9c2edd0234
Bogdan Jastrzębski <bjastrzebski@eden@mini.pw.edu.pl>
2023-02-21 22:41:21 +0100

    local changes

--- src/data_preparation/data_preparation.py
+++ src/data_preparation/data_preparation.py
@@ -11,13 +11,23 @@ def indexed_abstract_to_string(indexed_abstract):
 def authors_to_ids(authors):
     return [e['id'] for e in authors]
 
-name = ""
-path = ""
+print("Load data...")
+
+name = "dblp.v12"
+path = "../../data"
 
 cols = ['id', 'title', 'indexed_abstract', 'authors', 'references']
 
-df = pd.read_json(f"{path}/{name}.json")
+df = pd.read_json(f'{path}/{name}.json')
+
+print("Filter...") 
 df = df[~df[cols].isna().any(axis=1)]
+
+print("Abstract...")
 df['abstract']   = df.indexed_abstract.map(indexed_abstract_to_string)
+
+print("Author ids...")
 df['author_ids'] = df.authors.map(authors_to_ids)
+
+print("Saving...")
 df[['id', 'author_ids', 'title', 'abstract', 'references']].to_parquet(f'{path}/{name}.parquet')
